020a93d7974970330fd20e9621410447f350ab3a
Elsa Rrustaj <elsa.rrustaj@vodafone.com>
2026-02-04 14:11:26 +0100

    .

--- nextproject/app/components/layoutapp/footer/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="w-full bg-gray-800 p-4 text-white mt-8">
-      <p className="text-center">&copy; 2026 BlogPosts. All rights reserved.</p>
-    </footer>
-  );
-}
